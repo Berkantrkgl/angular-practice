8cf345ef0de47294be57b3d2423950bf7be2b8a3
brkntrkgl <berkan.turkoglu@hf-turkey.com>
2024-08-06 15:39:24 +0300

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { DocsComponent } from './docs/docs.component';
+import { EditComponent } from './edit/edit.component';
+import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './home/content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'aboutus',
+        component: AboutComponent,
+    }, 
+    {
+        path: 'docs',
+        component: DocsComponent,
+    },
+    {
+        path: 'edit',
+        component: EditComponent,
+    }, 
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {
+                path: 'content/:name',
+                component: ContentComponent,
+                pathMatch: 'full',
+            }
+        ]
+    }
+];
