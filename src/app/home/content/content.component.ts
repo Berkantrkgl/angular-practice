df4a6accaec6447ffbddef03b6b9af1795855183
brkntrkgl <berkan.turkoglu@hf-turkey.com>
2024-08-06 16:16:16 +0300

    inital commit

--- src/app/home/content/content.component.ts
+++ src/app/home/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
-
+export class ContentComponent implements OnInit{
+  name = input.required<string>();
+  
+  ngOnInit(): void {
+    console.log(this.name)
+  }
 }
